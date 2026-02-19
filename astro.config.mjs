9828146252425fc931c44895427a893bcd1a3397
Cristóbal Ramos Merino <cristobalramosmerino@gmail.com>
2026-02-19 18:15:15 -0300

    feat: implement solidjs and seo utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://hipersticion.cl",
+    base: "/",
+    integrations: [mdx(), sitemap(), solidJs()],
+});
