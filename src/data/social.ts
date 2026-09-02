3bf68fa7768b235ef5a7c64ea35405407e01c95a
Cristóbal Ramos Merino <cristobalramosmerino@gmail.com>
2026-09-02 11:12:43 -0400

    feat: agregar enlaces a redes sociales en el footer

---
+++ src/data/social.ts
@@ -0,0 +1,5 @@
+export const socialLinks = [
+    { label: "Instagram", href: "https://www.instagram.com/actuelmarx/" },
+    { label: "Facebook", href: "https://www.facebook.com/actuelmarxintervenciones/" },
+    { label: "Mastodon", href: "https://lile.cl/@actuelmarxint" },
+];
